c7e01bf353b98006ee244cb412acb003a5fef75e
Jordan Dysart <jordandysart@live.ca>
2020-01-20 01:25:39 -0600

    added some components to use for writing process

--- app_builder/utils/template_writer.py
+++ app_builder/utils/template_writer.py
@@ -13,37 +13,78 @@
 # Also, why don't I just copy the template into output and replace lines directly
 # in the one file.
 
+import re
+
 from os import path, getenv
 from shutil import copyfile
+
 
 PROJECT_DIR  = getenv('PROJECT_DIR')
 APP_DIR      = 'app_builder/'
 OUTPUT_DIR   = path.join(PROJECT_DIR, APP_DIR, 'output/')
 TEMPLATE_DIR = path.join(PROJECT_DIR, APP_DIR, 'templates/')
+REXPRESS     = r".*\{\{(.*)\}\}"
+WORK_FILE    = path.join(OUTPUT_DIR, "planned_work")
 
+class TemplateWriter:
 
-input_file = None
+    def __init__(self, library):
+        self.library = library
+        self.work = []
 
-def setInputFileName(filename):
-    '''
-    set the input path name
-    '''
-    input_file = path.join(TEMPLATE_DIR, str(filename))
-    return path.isfile(input_file)
+    def templateExists(self, filename):
+        '''
+        set the template path name
+        '''
+        input_file = path.join(TEMPLATE_DIR, str(filename))
+        return path.isfile(input_file)
 
-def copyTemplateToOutput(filename):
-    if input_file:
-        output_file = path.join(OUTPUT_DIR, filename)
-        copyfile(input_file, output_file)
+    def copyTemplateToOutput(self, template, output):
+        '''
+        I'm going to copy the desired tempolate to output before planning or doing work
+        '''
+        template_file = path.join(TEMPLATE_DIR, template)
+        output_file = path.join(OUTPUT_DIR, output)
+        copyfile(template_file, output_file)
         output_file = None
 
 
-def checkLine(line):
-    '''
-    take a line from a filereader and test if there is template placeholder
-    {% like this %}. If there is start parsing the text inside.
-    '''
-    return False
+    def checkLine(self, line):
+        '''
+        take a line from a filereader and test if there is template placeholder
+        {{} like this }}. return the match, it's None if there is no match
+        '''
+        expression = re.compile(REXPRESS)
+        match = expression.search(line)
+        return match
+
+    def planWork(self, line_num, work_found):
+        self.work.append(f"{line_num}:{work_found}")
+    
+    def prepLibrary(self):
+        work_string = ''
+        for work_order in self.work:
+            _, which = work_order.split(':')
+            
+            if which == "categories":
+                for category in self.library.getCategory():
+                    work_string += f'\"{category }\",\n\t\t'
+            elif which == 'icons':
+                work_string += 'do nothing for now\n'
+            elif which == 'words':
+                self.library.getLibrary()
+                
+            elif which == 'audio':
+                print(self.library.getLibrary())
+
+
+        return work_string
+            
+        
+
+
+
+    
     
 
 
--- tests/test_filer.py
+++ tests/test_filer.py
@@ -11,8 +11,13 @@
 # Remarks : This is being built for android java right now
 
 import unittest, context
+from unittest import mock
 
+# replace these, and mock them
 from os.path import isfile, join
+from os import getcwd
+
+
 
 from app_builder.utils.template_writer import *
 from app_builder.models.word import Word
@@ -28,41 +33,104 @@ class FilerTests(unittest.TestCase):
         '''
         words = [ Word(f"word_{count+1}") for count in range(10) ]
         categories = [ Category(f"category_{count+1}") for count in range(10) ]
-        self.library = Library()
+        library = Library()
         for category in categories:
-            self.library.addCategory(category)
+            library.addCategory(category)
             for word in words:
                 category.addWord(word)
+        self.tw = TemplateWriter(library)
+        self.testtemplate = "tests/test_template.txt"
 
-    def test_set_input_filename(self):
-        self.assertEqual(True, setInputFileName('testfile.txt'))
-        self.assertEqual(False, setInputFileName('doesnotexist.txt'))
+    def test_template_filename(self):
+        '''
+        TODO: change this guy, the test depends on whether or not a file exists.
+        '''
+        self.assertEqual(True, self.tw.templateExists('testfile.txt'))
+        self.assertEqual(False, self.tw.templateExists('doesnotexist.txt'))
 
-    def test_set_input_with_integer(self):
-        self.assertEqual(False, setInputFileName(123))
+    def test_template_with_integer(self):
+        self.assertEqual(False, self.tw.templateExists(123))
 
     def test_copy_template_to_output(self):
         output_file = 'outfile.java'
         output_file_path = join(OUTPUT_DIR, output_file)
 
-        self.assertEqual(False, isfile(output_file_path))
-        setInputFileName('testfile.txt')
-        copyTemplateToOutput(output_file)
+        input_file = 'testfile.txt'
+        if self.tw.templateExists(input_file):
+            self.tw.copyTemplateToOutput(input_file,output_file)
         self.assertEqual(True, isfile(output_file_path))
-        if (isfile(output_file_path) and False):
-            from os import remove
-            remove(output_file_path)
 
-
-    def test_check_line(self):
+    def test_check_line_false(self):
         test_line = 'import java.util.Collections;'
-        self.assertEqual(False, checkLine(test_line))
+        self.assertEqual(None, self.tw.checkLine(test_line))
+
+    def test_check_line_true(self):
+        test_line = "{{[f'R.raw.{icon},' for icon in icons]}}"
+        self.assertIsNot(None,self.tw.checkLine(test_line))
     
-   
+    def test_plan_work(self):
+
+        test_line = "{{[f'{category},' for category in categories]}}"
+        self.assertEqual(None, self.tw.planWork(1,test_line))
+
+    def test_get_work_category(self):
+        category = Category("test_work")
+        library = Library()
+        library.addCategory(category)
+        template_writer = TemplateWriter(library)
+        test_data = ['6',"categories"]
+        template_writer.planWork(*test_data)
+
+        expected_result = '"test_work",\n\t\t'
+        self.assertEqual(expected_result, template_writer.prepLibrary())
+
+    def test_get_work_category_content(self):
+        category = Category("test_work")
+        word = Word('Jordan.mp3')
         
+        category.addWord(word)
+        word = Word('Dysart.mp3')
+        category.addWord(word)
+        library = Library()
+        library.addCategory(category)
+        template_writer = TemplateWriter(library)
+        test_data = ['6',"words"]
+        template_writer.planWork(*test_data)
+
+        expected_result = '"jordan",\n\t\t"dysart",'
+        self.assertEqual(expected_result, template_writer.prepLibrary())
 
 
+        
+    # def test_get_work_category_content_audio(self):
+    #     category = Category("test_work")
+    #     word = Word('jordan')
+    #     category.addWord(word)
+    #     word = Word('dysart')
+    #     category.addWord(word)
+    #     library = Library()
+    #     library.addCategory(category)
+    #     template_writer = TemplateWriter(library)
+    #     test_data = ['6',"audio"]
+    #     template_writer.planWork(*test_data)
 
+        
 
 if __name__ == "__main__":
     unittest.main()
+
+# Garbage Dump
+
+
+        # output_file = path.join(getcwd(), self.testtemplate)
+        
+        # with open(output_file, 'r') as template:
+        #     for num, line in enumerate(template):
+        #         print(line, end='')
+        #         match = self.tw.checkLine(line)
+        #         if match != None:
+        #             self.tw.planWork(num, match.group(1))
+        #             break
+            
+        #     with open(WORK_FILE, 'r') as work:
+        #         self.assertEqual('12 List<Categories>\n', work.readline())
